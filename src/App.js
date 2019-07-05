6c0bb967c014c272685c56c65ae9113bc1c6eadd
unknown <sinha.aditya244@gmail.com>
2019-07-05 20:32:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+import {NavLink, Route} from 'react-router-dom';
+import SignIn from './Components/SignIn/SignIn';
+import SignUp from './Components/SignUp/SignUp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <div className="app_div">
+          </div>
+          <div className="form_div">
+            <div className="nav_items">
+              <NavLink exact to = "/" activeClassName = "button_active" className="sign_in">Sign In </NavLink>
+              <NavLink to ="/sign-up" activeClassName = "button_active" className="sign_up">Sign Up </NavLink>
+
+              <Route path="/" exact component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
